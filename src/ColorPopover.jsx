82dbdf7b3688e6b1263512abf961376d05031688
sahilVishnaniLog <sahilvishnani10@gmail.com>
2025-11-14 14:45:32 +0530

    the colorbackground has now radio capablities and highlights the color that is selected

--- src/ColorPopover.jsx
+++ src/ColorPopover.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import backgroundURLObject from "./color.js";
+import { useTheme } from "./ThemeContext.jsx";
 
 import {
   Box,
@@ -16,17 +17,46 @@ import {
 } from "@mui/material";
 
 export default function ColorPopover() {
+  const [chosenBackground, setChosenBackground] = React.useState("dark");
+  const handleSelect = (event) => {
+    console.log(event.target.value);
+    setChosenBackground(event.target.value);
+  };
   return (
     <>
-      <Grid container spacing={2}>
-        {backgroundURLObject("dark").map((item) => (
-          <Tooltip key={item.label} title={item.label}>
-            <Paper
-              sx={{ width: 30, height: 30, background: item.background }}
-            ></Paper>
-          </Tooltip>
-        ))}
-      </Grid>
+      <FormControl>
+        <RadioGroup value={chosenBackground} onChange={handleSelect}>
+          <Grid container spacing={2} bgcolor="white" padding={2}>
+            {backgroundURLObject("light").map((item) => (
+              <FormControlLabel
+                key={item.label}
+                value={item.background}
+                control={<Radio sx={{ display: "none" }} />}
+                label={
+                  <Tooltip key={item.label} title={item.label}>
+                    <Paper
+                      sx={{
+                        width: 60,
+                        height: 60,
+                        background: item.background,
+                        border:
+                          chosenBackground === item.background
+                            ? "2px solid #000000"
+                            : "none",
+                        "&:hover": {
+                          border: "2px solid #000000",
+                        },
+                      }}
+                    ></Paper>
+                  </Tooltip>
+                }
+              />
+            ))}
+            <Typography>No background</Typography>
+          </Grid>
+        </RadioGroup>
+      </FormControl>
     </>
   );
 }
+``;
